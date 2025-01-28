03522fcebf96e2e0b482a4e384137cd70069ae9c
Oddant1 <oddant1@hotmail.com>
2025-01-28 15:11:10 -0700

    Citations refactored

--- src/lib/models/citationsModel.ts
+++ src/lib/models/citationsModel.ts
@@ -1,27 +1,35 @@
 import Cite from "citation-js";
 
+import JSZip, { JSZipObject } from "jszip";
+
 import asmTemplate from "$lib/citation-templates/asm";
 import cellTemplate from "$lib/citation-templates/cell";
 import chicagoTemplate from "$lib/citation-templates/chicago";
 import mlaTemplate from "$lib/citation-templates/mla";
 import natureTemplate from "$lib/citation-templates/nature";
 
-export default class FormatterModel {
+class CitationsModel {
+  // Class attributes containing current state of the citations
   fileExt = "";
   citations = "";
   fileContents = "";
   downloadableFile = "";
   formattedCitations = "";
-
   citationStyle = "bib";
 
+  // Class attributes used to format the citations
   formatter = new Cite();
   register = Cite.CSL.register.addTemplate;
+
+  // Class attributes passed in by readerModel pertaining to currently loaded
+  // result
+  uuid = "";
+  zipReader: JSZip = new JSZip();
 
   //***************************************************************************
   // Start boilerplate to make this a subscribable svelte store
   //***************************************************************************
-  _subscription: Record<number, (arg0: FormatterModel) => void> = {};
+  _subscription: Record<number, (arg0: CitationsModel) => void> = {};
   _subscriptionNum = 0;
 
   _dirty() {
@@ -30,7 +38,7 @@ export default class FormatterModel {
     }
   }
 
-  subscribe(subscription: (value: FormatterModel) => void): () => void {
+  subscribe(subscription: (value: CitationsModel) => void): () => void {
     this._subscription[this._subscriptionNum] = subscription;
     subscription(this);
     return ((index) => {
@@ -51,9 +59,11 @@ export default class FormatterModel {
     this.register("nature", natureTemplate);
   }
 
-  setState(citations: string) {
-    this.citations = citations;
-    this.formatter = new Cite(citations);
+  // This state is set by the readerModel when it comes time to read the
+  // citations
+  setState(uuid: string, zipReader: JSZip) {
+    this.uuid = uuid;
+    this.zipReader = zipReader;
   }
 
   formatCitations() {
@@ -90,4 +100,67 @@ export default class FormatterModel {
     const blob = new Blob([this.fileContents], { type: "text/plain" });
     return URL.createObjectURL(blob);
   }
+
+  getCitations() {
+    this._getCitations().then((citations) => {
+      // If citations === null then we don't have citaions for this result
+      // which is fine
+      console.log(citations)
+      if (citations !== null) {
+        this.citations = this._dedup(citations);
+        this.formatter = new Cite(this.citations);
+
+        this._dirty();
+      }
+    });
+  }
+
+  _getCitations() {
+    if (this.zipReader.file(`${this.uuid}/provenance/citations.bib`) === null) {
+      return Promise.resolve(null);
+    }
+
+    const promises: Array<Promise<any>> = [];
+    const folder = this.zipReader.folder(`${this.uuid}/provenance`);
+
+    if (folder === null) {
+      throw new Error(`No folder found in Result matching ${this.uuid}/provenance`);
+    }
+
+    folder.forEach((relPath: string, file: JSZipObject) => {
+        if (relPath.endsWith("citations.bib")) {
+          promises.push(file.async("text"));
+        }
+      });
+
+    console.log(promises)
+    return Promise.all(promises).then((array) => array.join(""));
+  }
+
+  _dedup(bibtex: string) {
+    const store = {};
+    const dedup = [];
+
+    let skip = false;
+    for (const line of bibtex.split("\n")) {
+      if (line.startsWith("@")) {
+        skip = false;
+        const id = /@.*{(.*),\w*/.exec(line)[0];
+
+        if (id in store) {
+          skip = true;
+        } else {
+          store[id] = true;
+        }
+      }
+
+      if (!skip) {
+        dedup.push(line);
+      }
+    }
+    return dedup.join("\n");
+  }
 }
+
+const citationsModel = new CitationsModel();
+export default citationsModel;
