9282b83e15e4e2d19cdc79a88a0de471abbe5f07
Oddant1 <oddant1@hotmail.com>
2025-03-03 15:34:20 -0700

    At least make each its own div

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -5,22 +5,24 @@
 
 <div>
   {#each $provenanceModel.metadataMap.entries() as entry}
-    {entry[0]}
-    {#each entry[1] as inner}
-      {inner.file}
-      {inner.artifacts}
-      <button on:click={async () => {
-          const file = await getFile(`provenance/action/${inner.file}`, entry[0], provenanceModel.zipReader).then((data) => new Blob([data.byteArray], { type: data.type }))
-          const link = document.createElement('a');
+    <div>
+      {entry[0]}
+      {#each entry[1] as inner}
+        {inner.file}
+        {inner.artifacts}
+        <button on:click={async () => {
+            const file = await getFile(`provenance/action/${inner.file}`, entry[0], provenanceModel.zipReader).then((data) => new Blob([data.byteArray], { type: data.type }))
+            const link = document.createElement('a');
 
-          link.href = URL.createObjectURL(file);
-          link.download = inner.file;
-          link.click();
-          URL.revokeObjectURL(link.href);
-        }
-      }>
-        Download
-      </button>
-    {/each}
+            link.href = URL.createObjectURL(file);
+            link.download = inner.file;
+            link.click();
+            URL.revokeObjectURL(link.href);
+          }
+        }>
+          Download
+        </button>
+      {/each}
+    </div>
   {/each}
 </div>
