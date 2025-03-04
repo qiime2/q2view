14d21b34b787a03f291e834d09368b41b96c385f
Oddant1 <oddant1@hotmail.com>
2025-03-04 15:01:58 -0700

    Basic table

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -1,28 +1,42 @@
 <script lang="ts">
   import provenanceModel from "$lib/models/provenanceModel";
-    import { getFile } from "$lib/scripts/fileutils";
+  import { getFile } from "$lib/scripts/fileutils";
 </script>
 
-<div>
+<table>
+  <tr>
+    <th>Artifact UUID</th>
+    <th>File Name</th>
+    <th>Download</th>
+  </tr>
   {#each $provenanceModel.metadataMap.entries() as entry}
-    <div>
-      {entry[0]}
+    <tr>
+      <td>{entry[0]}</td>
       {#each entry[1] as inner}
-        {inner.file}
-        {inner.artifacts}
-        <button on:click={async () => {
-            const file = await getFile(`provenance/action/${inner.file}`, entry[0], provenanceModel.zipReader).then((data) => new Blob([data.byteArray], { type: data.type }))
-            const link = document.createElement('a');
+        <td>{inner.file}</td>
+        <td>
+          <button on:click={async () => {
+              const file = await getFile(`provenance/action/${inner.file}`, entry[0], provenanceModel.zipReader).then((data) => new Blob([data.byteArray], { type: data.type }))
+              const link = document.createElement('a');
 
-            link.href = URL.createObjectURL(file);
-            link.download = inner.file;
-            link.click();
-            URL.revokeObjectURL(link.href);
-          }
-        }>
-          Download
-        </button>
+              link.href = URL.createObjectURL(file);
+              link.download = inner.file;
+              link.click();
+              URL.revokeObjectURL(link.href);
+            }
+          }>
+            Download
+          </button>
+        </td>
       {/each}
-    </div>
+      </tr>
   {/each}
-</div>
+  </table>
+
+<style lang="postcss">
+  table {
+    @apply border
+    border-solid
+    border-gray-300;
+  }
+</style>
