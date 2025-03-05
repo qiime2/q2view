33e9577ea2c20c590a1845669449e4ee76ef20ef
Anthony Simard <oddant1@hotmail.com>
2025-03-05 10:10:12 -0700

    Update src/lib/components/Metadata.svelte
    Co-authored-by: Greg Caporaso <192372+gregcaporaso@users.noreply.github.com>

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -7,7 +7,7 @@
     import readerModel from "$lib/models/readerModel";
 </script>
 
-<Panel header="Note About Metadata:">
+<Panel header="Warning: Do not include confidential information in your metadata.">
   <p>
     QIIME 2 goes to great lengths to ensure that your bioinformatics workflow
     will be reproducible. This includes recording information about your analysis
