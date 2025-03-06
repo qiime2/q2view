cb7aabb6e0eb64bc18aa1b8e3220401c887ddb1f
Oddant1 <oddant1@hotmail.com>
2025-03-06 14:44:21 -0700

    Format

--- src/lib/tests/provenanceSearch.test.ts
+++ src/lib/tests/provenanceSearch.test.ts
@@ -6,7 +6,7 @@ import provenanceModel from "$lib/models/provenanceModel";
 const stringJSON = {
   uuid: "string",
   key: "42",
-  keyNum: 42
+  keyNum: 42,
 };
 
 const altStringJSON = {
@@ -78,71 +78,75 @@ const testMap = new BiMap(
     ["anchor", anchorJSON],
     ["escapedAnchors", escapedAnchorsJSON],
     ["reversedAnchors", reversedAnchorsJSON],
-  ])
+  ]),
 );
 provenanceModel.nodeIDToJSON = testMap;
 
 test("test string", () => {
-  const hits= searchProvenance('key: "42" OR key: "true" OR key: "false" OR key: "null"');
+  const hits = searchProvenance(
+    'key: "42" OR key: "true" OR key: "false" OR key: "null"',
+  );
   expect(hits.toString() === '["string"]');
-})
+});
 
 test("test number", () => {
   const hits = searchProvenance("key: 42");
   expect(hits.toString() === '["number"]');
-})
+});
 
 test("test true", () => {
   const hits = searchProvenance("key: true");
   expect(hits.toString() === '["true"]');
-})
+});
 
 test("test false", () => {
   const hits = searchProvenance("key: false");
   expect(hits.toString() === '["false"]');
-})
+});
 
 test("test null", () => {
   const hits = searchProvenance("key: null");
   expect(hits.toString() === '["null"]');
-})
+});
 
 test("test nested key", () => {
   const hits = searchProvenance('nested.key: "nested"');
   expect(hits.toString() === '["nested"]');
-})
+});
 
 test("test escaped key", () => {
   const hits = searchProvenance('escap\.ed.key: "escaped"');
   expect(hits.toString() === '["escaoed"]');
-})
+});
 
 test("test value list", () => {
   const hits = searchProvenance('key: (("4" AND "2") OR "3")');
   expect(hits.toString() === '["string", "altString"]');
-})
+});
 
 test("test query list", () => {
-  const hits = searchProvenance('(key: "42" AND keyNum: 42) OR nested.key: "nested"')
+  const hits = searchProvenance(
+    '(key: "42" AND keyNum: 42) OR nested.key: "nested"',
+  );
   expect(hits.toString() === '["string", "nested"]');
-})
+});
 
 test("test start anchor", () => {
   const hits = searchProvenance('anchorKey: "^start"');
   expect(hits.toString() === '["anchor"]');
-})
+});
 
 test("test end anchor", () => {
   const hits = searchProvenance('anchorKey: "end$"');
   expect(hits.toString() === '["anchor"]');
-})
+});
 
 test("test both anchors", () => {
   const hits = searchProvenance('anchorKey: "^start and end$"');
   expect(hits.toString() === '["anchor"]');
-})
+});
 
 test("test escaped anchors", () => {
   const hits = searchProvenance('anchorKey: "\^start and end\$"');
   expect(hits.toString() === '["escapedAnchors"]');
-})
+});
