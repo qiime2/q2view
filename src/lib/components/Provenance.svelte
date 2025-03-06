5a537d1bc10b0f87fd676beee27ddf3cfa2b48dd
Oddant1 <oddant1@hotmail.com>
2025-03-05 17:28:12 -0700

    More text formatting. Still don't really like it

--- src/lib/components/Provenance.svelte
+++ src/lib/components/Provenance.svelte
@@ -22,24 +22,33 @@
           />
         </div>
       {:else}
-        <p class="text-sm pb-2">Click on an element of the Provenance Graph to learn more. Alternatively, you can search the prov dag for nodes matching specific criteria.</p>
-        <p class="text-md">Search Query Instructions:</p>
-        <p class="text-sm">
-          All search queries are of the form key: value <br><br>
-          If the value you are searching for is a string (appears in quotes in the Details panel) it must be in quotes in your search query e.g. type: "FeatureData" <br><br>
-          String values match on includes and all other values match on equality. <br><br>
-          This means type: "FeatureData" will match all type's containing "FeatureData" while sampling_depth: 1000 will only match sampling depths equaling exactly 1000 <br><br>
-          Note the values false, true, and null are often NOT strings and do not need quotes <br><br>
-          Strings can use the start and end of string anchors "^" and "$"" <br><br>
-          type: "^FeatureData" will match all type's that start with "FeatureData" while type: "FeatureData$" will match all types that end with "FeatureData" <br><br>
-          Note that this means you can match an exact string by beginning your search string with "^" and ending it with "$" e.g. type: "^FeatureData[Taxonomy]$" will only match exactly "FeatureData[Taxonomy]". Although in this case the anchors are likely unnecessary because any type that includes "FeatureData[Taxonomy]" is probably exactly "FeatureData[Taxonomy]" <br><br>
-          If your search value actually starts with "^" or ends with "$" you will have to escape them with "\" e.g. key: "\^String starts and ends with anchors\$"<br><br>
-          You can also combine values with AND and OR e.g. uuid: (("^6" AND "$5") OR "ee") <br><br>
-          You can do the same with multiple keys e.g. uuid: "6" OR (sampling_depth: 1000 AND other: false) <br><br>
-          You can specify multiple levels of key by seperating them with . e.g. execution.uuid: "6" <br><br>
-          The same as with anchors, if your key contains a . you must escape it with \ e.g. key.contains.per\.iod: "value"
-        </p>
+        <div class="text-gray-800">
+          <p class="pb-3 leading-5">Click on an element of the Provenance Graph to learn more. Alternatively, you can search provenance for actions and results matching specific criteria.</p>
+          <p class="font-bold">Search Query Instructions:</p>
+          <p class="leading-5">All search queries are of the form key: "value" ex:</p><span class="example">uuid: "&lt;uuid&gt;"</span>
+          <p class="pt-3 font-bold">Rules for Keys:</p>
+          <p class="leading-5">You can specify multiple levels of key by seperating them with "."" ex:</p><span class="example">execution.uuid: "&lt;uuid&gt;"</span>
+          <p class="pt-3 leading-5">If your key contains a "." you must escape it with "\" ex:</p><span class="example">key.contains.per\.iod: "value"</span>
+          <p class="pt-3 leading-5">You can combine multiple keys with AND and OR ex:</p><span class="example">uuid: "&lt;uuid&gt;" OR (trunc_len: 150 AND hashed_feature_ids: true)</span>
+          <p class="pt-3 font-bold">Rules for Values:</p>
+          <p>If the value you are searching for is a string (if it is a string it will appear in quotes in this panel) it must be in quotes in your search query ex:</p><span class="example">type: "FeatureData"</span>
+          <p class="pt-3 leading-5">String values match on includes and all other values match on equality ex:</p><span class="example">type: "FeatureData"</span><p class="leading-5">will match all types containing "FeatureData"</p><span class="example">sampling_depth: 1000</span><p class="leading-5">will only match sampling depths equaling exactly 1000</p>
+          <p class="pt-3 leading-5"><span class="font-bold">Note:</span> the values true, false, and null are often NOT strings and do not need quotes </p>
+          <p class="pt-3 leading-5">Strings can use the start and end of string anchors "^" and "$" ex:</p><span class="example">type: "^FeatureData"</span><p class="leading-5">will match all types that start with "FeatureData"</p><span class="example">type: "FeatureData$"</span><p class="leading-5">will match all types that end with "FeatureData"</p><span class="example">type: "^FeatureData[Taxonomy]$"</span><p class="leading-5">will only match exactly "FeatureData[Taxonomy]"</p>
+          <p class="pt-3 leading-5">If your search value actually starts with "^" or ends with "$" you will have to escape them with "\" ex:</p><span class="example">key: "\^String starts and ends with anchors\$"</span>
+          <p class="pt-3 leading-5">You can also combine values with AND and OR ex:</p><span class="example">uuid: (("^6" AND "$5") OR "ee")</span>
+        </div>
       {/if}
     </Panel>
   </div>
 {/key}
+
+<style lang="postcss">
+  .example {
+    @apply text-black
+    bg-gray-300
+    rounded-sm
+    px-1
+    py-0.5;
+  }
+</style>
