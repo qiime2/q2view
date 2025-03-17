49b00e6d77152088fabc7f936419b01edcce3724
Oddant1 <oddant1@hotmail.com>
2025-03-17 16:13:38 -0700

    update query instructions

--- src/lib/components/Provenance.svelte
+++ src/lib/components/Provenance.svelte
@@ -25,7 +25,8 @@
         <div class="text-gray-700 text-sm">
           <p class="pb-3 leading-5">Click on an element of the Provenance Graph to learn more. Alternatively, you can search the graph for actions and results matching specific criteria</p>
           <p class="font-bold text-lg">Search Query Instructions:</p>
-          <p class="leading-5">All search queries are of the form key: "value"</p><span class="example">uuid: "&lt;uuid&gt;"</span>
+          <p class="leading-5">To search for a given key with a given value use the 'key: value' syntax</p><span class="example">sampling_depth: 1000</span>
+          <p class="leading-5">To search for a given key with any value simply give the key on its own</p><span class="example">sampling_depth</span>
           <p class="pt-3 font-bold text-lg">Rules for Keys:</p>
           <p class="leading-5">You can specify multiple levels of key by seperating them with "." </p><span class="example">execution.uuid: "&lt;uuid&gt;"</span>
           <p class="pt-3 leading-5">If your key contains a "." you must escape it with "\" </p><span class="example">key.contains.per\.iod: "value"</span>
@@ -33,10 +34,11 @@
           <p class="pt-3 font-bold text-lg">Rules for Values:</p>
           <p class="leading-5">If the value you are searching for is a string it must be in quotes</p><span class="example">type: "FeatureData"</span>
           <p class="pt-3 leading-5"><span>Note:</span> the values true, false, and null are often NOT strings and do not need quotes </p>
-          <p class="pt-3 leading-5">Strings match on includes and all other types match on equality </p><span class="example">type: "FeatureData"</span><p class="leading-5">will match all types containing "FeatureData"</p><span class="example">sampling_depth: 1000</span><p class="leading-5">will only match sampling depths equaling exactly 1000</p>
-          <p class="pt-3 leading-5">Strings can use the start and end of string anchors "^" and "$" </p><span class="example">type: "^FeatureData"</span><p class="leading-5">will match all types that start with "FeatureData"</p><span class="example">type: "FeatureData$"</span><p class="leading-5">will match all types that end with "FeatureData"</p><span class="example">type: "^FeatureData[Taxonomy]$"</span><p class="leading-5">will only match exactly "FeatureData[Taxonomy]"</p>
+          <p class="pt-3 leading-5">Strings match on includes and all other types match on equality</p><span class="example">type: "FeatureData"</span><p class="leading-5">will match all types containing "FeatureData"</p><span class="example">sampling_depth: 1000</span><p class="leading-5">will only match sampling depths equaling exactly 1000</p>
+          <p class="pt-3 leading-5">Strings can use the start and end of string anchors "^" and "$"</p><span class="example">type: "^FeatureData"</span><p class="leading-5">will match all types that start with "FeatureData"</p><span class="example">type: "FeatureData$"</span><p class="leading-5">will match all types that end with "FeatureData"</p><span class="example">type: "^FeatureData[Taxonomy]$"</span><p class="leading-5">will only match exactly "FeatureData[Taxonomy]"</p>
           <p class="pt-3 leading-5">If your search value starts with "^" or ends with "$" you will have to escape them with "\" </p><span class="example">key: "\^String starts and ends with anchors\$"</span>
-          <p class="pt-3 leading-5">You can also combine values with AND and OR </p><span class="example">uuid: (("^6" AND "5$") OR "ee")</span>
+          <p class="pt-3 leading-5">Numerical values can use the comparison operators &gt;, &gt;=, &lt;, &lt;=</p><span class="example">sampling_depth: &gt;=1000</span><p class="leading-5">will match all sampling_depths greater than or equal to 1000</p>
+          <p class="pt-3 leading-5">You can also combine values with AND and OR </p><span class="example">uuid: (("^6" AND "5$") OR "ee")</span><p class="leading-5">Will match all uuids that start with 6 and end with 5 or contain "ee"</p>
         </div>
       {/if}
     </Panel>
