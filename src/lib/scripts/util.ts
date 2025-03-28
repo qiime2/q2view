0b82c4fe31e17a446978e55963c6ad751414c180
Oddant1 <oddant1@hotmail.com>
2025-03-28 15:45:05 -0700

    obect and undefined are mutually exclusive

--- src/lib/scripts/util.ts
+++ src/lib/scripts/util.ts
@@ -129,12 +129,12 @@ export function getAllObjectKeysRecursively(
     for (const key of Object.keys(targetObject)) {
       const newKeyAccumulator = [...currentkeyAccumulator, key];
 
-      // Some terminal values, such as the start and end times, will parse as objects, but they do not have keys of their own
+      // Some terminal values, such as the start and end times, will parse as
+      // objects, but they do not have keys of their own
       const next = targetObject[key as keyof object];
       if (
         typeof next === "object" &&
         next !== null &&
-        next !== undefined &&
         Object.keys(next).length !== 0
       ) {
         getAllObjectKeysRecursively(next, newKeyAccumulator, globalKeySet);
