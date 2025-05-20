9a0d335e48e2099e7e7c16e26e8ca887b027153f
Oddant1 <oddant1@hotmail.com>
2025-05-20 14:38:59 -0700

    merge pop

--- src/lib/scripts/biMap.ts
+++ src/lib/scripts/biMap.ts
@@ -3,13 +3,22 @@ export default class BiMap<T, P> {
   keyToValue: Map<T, P>;
   valueToKey: Map<P, T>;
 
-  constructor(keyToValue: Map<T, P> | undefined = undefined) {
+  // jsonify if true will jsonify objects passed to get and set and rehydrate
+  // objects returned from set
+  constructor(keyToValue: Map<T, P> | undefined = undefined, jsonify=false) {
     if (keyToValue === undefined) {
       this.keyToValue = new Map();
       this.valueToKey = new Map();
     } else {
       this.keyToValue = keyToValue;
       this.valueToKey = this._reverseMap(keyToValue);
+    }
+
+    if (jsonify) {
+      this.set = this._setJSON;
+      this.get = this._getJSON;
+      this.getKey = this._getKeyJSON;
+      this.keys
     }
   }
 
@@ -57,5 +66,26 @@ export default class BiMap<T, P> {
   clear() {
     this.keyToValue.clear();
     this.valueToKey.clear();
+  }
+
+  _setJSON(key: T, value: P) {
+    this.keyToValue.set(key, value);
+    this.valueToKey.set(value, key);
+  }
+
+  _getJSON(key: T): P | undefined {
+    return this.keyToValue.get(key);
+  }
+
+  _getKeyJSON(value: P): T | undefined {
+    return this.valueToKey.get(value);
+  }
+
+  _keysJSON(): MapIterator<T> {
+    return this.keyToValue.keys();
+  }
+
+  _valuesJSON(): MapIterator<P> {
+    return this.keyToValue.values();
   }
 }
