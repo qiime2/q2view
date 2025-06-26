d533390f6a706625872cda3af6681f22ef7250cb
Oddant1 <oddant1@hotmail.com>
2025-06-26 14:04:04 -0700

    Remove other dead vars

--- src/lib/components/Gallery.svelte
+++ src/lib/components/Gallery.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
   import GalleryCard from "$lib/components/GalleryCard.svelte";
   import { onMount } from 'svelte';
-  // const GALLERY_URL = "https://q2view-gallery.pages.dev/gallery/";
-  // const GALLERY_URL = "https://github.com/Oddant1/library-plugins/tree/add-view-gallery/gallery";
+
   const GALLERY_URL = "https://raw.githubusercontent.com/Oddant1/library-plugins/refs/heads/add-view-gallery/gallery/"
 
   let galleryEntries: Array<Object> = [];
