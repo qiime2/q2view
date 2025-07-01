37b3f6c1318c4b7d51753af3a61fa684ea7b4e60
Oddant1 <oddant1@hotmail.com>
2025-07-01 16:48:27 -0700

    Add DAG wide error tab

--- src/lib/components/Provenance.svelte
+++ src/lib/components/Provenance.svelte
@@ -15,6 +15,14 @@
 
     return low + medium + high;
   }
+
+  function _sumGlobalErrors() {
+    const low = readerModel.provenanceModel.lowSeverityErrors.size;
+    const medium = readerModel.provenanceModel.medSeverityErrors.size;
+    const high = readerModel.provenanceModel.highSeverityErrors.size;
+
+    return low + medium + high;
+  }
 </script>
 
 {#key $readerModel.provenanceModel.uuid}
@@ -86,26 +94,92 @@
         </div>
       {/if}
     {:else}
-      <div class="text-gray-700 text-sm p-4">
-        <p class="pb-3 leading-5">Click on an element of the Provenance Graph to learn more. Alternatively, you can search the graph for actions and results matching specific criteria</p>
-        <p class="font-bold text-lg">Search Query Instructions:</p>
-        <p class="leading-5">To search for a given key with a given value use the 'key: value' syntax</p><span class="example">sampling_depth: 1000</span>
-        <p class="leading-5">To search for a given key with any value simply give the key on its own</p><span class="example">sampling_depth</span>
-        <p class="pt-3 leading-5"><span class="font-bold">NOTE: </span>The AND and OR operators described below are simply computed left to right. For complex queries, please use parentheses to indicate precedence.</p>
-        <p class="pt-3 font-bold text-lg">Rules for Keys:</p>
-        <p class="leading-5">You can specify multiple levels of key by seperating them with "."</p><span class="example">execution.uuid: "&lt;uuid&gt;"</span>
-        <p class="pt-3 leading-5">If your key contains a "." you must escape it with "\"</p><span class="example">key.contains.per\.iod: "value"</span>
-        <p class="pt-3 leading-5">You can combine multiple keys with AND and OR</p><span class="example">uuid: "&lt;uuid&gt;" OR (trunc_len: 150 AND hashed_feature_ids: true)</span>
-        <p class="pt-3 font-bold text-lg">Rules for Values:</p>
-        <p class="leading-5">If the value you are searching for is a string it must be in quotes</p><span class="example">type: "FeatureData"</span>
-        <p class="pt-3 leading-5"><span class="font-bold">NOTE:</span> the values true, false, and null are often NOT strings and do not need quotes</p>
-        <p class="pt-3 leading-5">Strings match on includes and all other types match on equality</p><span class="example">type: "FeatureData"</span><p class="leading-5">will match all types containing "FeatureData"</p><span class="example">sampling_depth: 1000</span><p class="leading-5">will only match sampling depths equaling exactly 1000</p>
-        <p class="pt-3 leading-5">Strings can use the start and end of string anchors "^" and "$"</p><span class="example">type: ^"FeatureData"</span><p class="leading-5">will match all types that start with "FeatureData"</p><span class="example">type: "[Taxonomy]"$</span><p class="leading-5">will match all types that end with "[Taxonomy]"</p><span class="example">type: ^"FeatureData[Taxonomy]"$</span><p class="leading-5">will only match exactly "FeatureData[Taxonomy]"</p>
-        <p class="pt-3 leading-5">If your search value contains a double quote, you will need to escape it with "\"</p><span class="example">type: "\""</span><p class="leading-5">will match all types that contain a double quote</p>
-        <p class="pt-3 leading-5">The same is true if your string contains a "\"</p><span class="example">type: "\\"</span><p class="leading-5">will match all types that contain a backslash</p>
-        <p class="pt-3 leading-5">Numerical values can use the comparison operators &gt;, &gt;=, &lt;, &lt;=</p><span class="example">sampling_depth: &gt;=1000</span><p class="leading-5">will match all sampling_depths greater than or equal to 1000</p>
-        <p class="pt-3 leading-5">You can also combine values with AND and OR. You must wrap these clauses in parentheses</p><span class="example">uuid: ((^"6" AND "5"$) OR "ee")</span><p class="leading-5">will match all uuids that start with 6 and end with 5 or contain "ee"</p>
-      </div>
+      {#if readerModel.provenanceModel.nodeIDToErrors.size > 0}
+        <div class="flex border-b border-solid border-gray-300 pb-2 pt-4 px-4 mb-4">
+          <button onclick={() => readerModel.provenanceModel.provTab = "provenance"} class="nav-button float-left mx-auto w-1/2 pb-0.5 {readerModel.provenanceModel.provTab === "provenance" ? "selected-nav-button" : ""}">
+            Provenance
+          </button>
+          <button onclick={() => readerModel.provenanceModel.provTab = "error"} class="nav-button float-right mx-auto w-1/2 pb-0.5 {readerModel.provenanceModel.provTab === "error" ? "selected-nav-button" : ""}">
+            Errors <span class="nav-button-child border border-gray-500 border-solid px-1.5 rounded-full">{_sumGlobalErrors()}</span>
+          </button>
+        </div>
+        <div class="text-gray-700 text-sm {readerModel.provenanceModel.provTab === "provenance" ? "block" : "hidden"} px-4">
+          <p class="pb-3 leading-5">Click on an element of the Provenance Graph to learn more. Alternatively, you can search the graph for actions and results matching specific criteria</p>
+          <p class="font-bold text-lg">Search Query Instructions:</p>
+          <p class="leading-5">To search for a given key with a given value use the 'key: value' syntax</p><span class="example">sampling_depth: 1000</span>
+          <p class="leading-5">To search for a given key with any value simply give the key on its own</p><span class="example">sampling_depth</span>
+          <p class="pt-3 leading-5"><span class="font-bold">NOTE: </span>The AND and OR operators described below are simply computed left to right. For complex queries, please use parentheses to indicate precedence.</p>
+          <p class="pt-3 font-bold text-lg">Rules for Keys:</p>
+          <p class="leading-5">You can specify multiple levels of key by seperating them with "."</p><span class="example">execution.uuid: "&lt;uuid&gt;"</span>
+          <p class="pt-3 leading-5">If your key contains a "." you must escape it with "\"</p><span class="example">key.contains.per\.iod: "value"</span>
+          <p class="pt-3 leading-5">You can combine multiple keys with AND and OR</p><span class="example">uuid: "&lt;uuid&gt;" OR (trunc_len: 150 AND hashed_feature_ids: true)</span>
+          <p class="pt-3 font-bold text-lg">Rules for Values:</p>
+          <p class="leading-5">If the value you are searching for is a string it must be in quotes</p><span class="example">type: "FeatureData"</span>
+          <p class="pt-3 leading-5"><span class="font-bold">NOTE:</span> the values true, false, and null are often NOT strings and do not need quotes</p>
+          <p class="pt-3 leading-5">Strings match on includes and all other types match on equality</p><span class="example">type: "FeatureData"</span><p class="leading-5">will match all types containing "FeatureData"</p><span class="example">sampling_depth: 1000</span><p class="leading-5">will only match sampling depths equaling exactly 1000</p>
+          <p class="pt-3 leading-5">Strings can use the start and end of string anchors "^" and "$"</p><span class="example">type: ^"FeatureData"</span><p class="leading-5">will match all types that start with "FeatureData"</p><span class="example">type: "[Taxonomy]"$</span><p class="leading-5">will match all types that end with "[Taxonomy]"</p><span class="example">type: ^"FeatureData[Taxonomy]"$</span><p class="leading-5">will only match exactly "FeatureData[Taxonomy]"</p>
+          <p class="pt-3 leading-5">If your search value contains a double quote, you will need to escape it with "\"</p><span class="example">type: "\""</span><p class="leading-5">will match all types that contain a double quote</p>
+          <p class="pt-3 leading-5">The same is true if your string contains a "\"</p><span class="example">type: "\\"</span><p class="leading-5">will match all types that contain a backslash</p>
+          <p class="pt-3 leading-5">Numerical values can use the comparison operators &gt;, &gt;=, &lt;, &lt;=</p><span class="example">sampling_depth: &gt;=1000</span><p class="leading-5">will match all sampling_depths greater than or equal to 1000</p>
+          <p class="pt-3 leading-5">You can also combine values with AND and OR. You must wrap these clauses in parentheses</p><span class="example">uuid: ((^"6" AND "5"$) OR "ee")</span><p class="leading-5">will match all uuids that start with 6 and end with 5 or contain "ee"</p>
+        </div>
+        <div class="{$readerModel.provenanceModel.provTab === "error" ? "block" : "hidden"} px-4">
+          {#if readerModel.provenanceModel.highSeverityErrors.size > 0}
+            <span class="font-bold">High Severity Errors</span><br>
+            {#each readerModel.provenanceModel.highSeverityErrors as error}
+              <div class="mb-2 bg-gray-200 rounded-lg p-2">
+                <span class="font-bold">name: </span> {error.name}<br>
+                <span class="font-bold">description: </span> {error.description}<br>
+                <span class="font-bold">discovery date: </span> {error.date}<br>
+                <span class="font-bold">query: </span> {error.query}<br>
+              </div>
+            {/each}
+          {/if}
+          {#if readerModel.provenanceModel.medSeverityErrors.size > 0}
+            <span class="font-bold">Medium Severity Errors</span><br>
+            {#each readerModel.provenanceModel.medSeverityErrors as error}
+              <div class="mb-2 bg-gray-200 rounded-lg p-2">
+                <span class="font-bold">name: </span> {error.name}<br>
+                <span class="font-bold">description: </span> {error.description}<br>
+                <span class="font-bold">discovery date: </span> {error.date}<br>
+                <span class="font-bold">query: </span> {error.query}<br>
+              </div>
+            {/each}
+          {/if}
+          {#if readerModel.provenanceModel.lowSeverityErrors.size > 0}
+            <span class="font-bold">Low Severity Errors</span><br>
+            {#each readerModel.provenanceModel.lowSeverityErrors as error}
+              <div class="mb-2 bg-gray-200 rounded-lg p-2">
+              <span class="font-bold">name: </span> {error.name}<br>
+                <span class="font-bold">description: </span> {error.description}<br>
+                <span class="font-bold">discovery date: </span> {error.date}<br>
+                <span class="font-bold">query: </span> {error.query}<br>
+              </div>
+            {/each}
+          {/if}
+        </div>
+      {:else}
+        <div class="text-gray-700 text-sm p-4">
+          <p class="pb-3 leading-5">Click on an element of the Provenance Graph to learn more. Alternatively, you can search the graph for actions and results matching specific criteria</p>
+          <p class="font-bold text-lg">Search Query Instructions:</p>
+          <p class="leading-5">To search for a given key with a given value use the 'key: value' syntax</p><span class="example">sampling_depth: 1000</span>
+          <p class="leading-5">To search for a given key with any value simply give the key on its own</p><span class="example">sampling_depth</span>
+          <p class="pt-3 leading-5"><span class="font-bold">NOTE: </span>The AND and OR operators described below are simply computed left to right. For complex queries, please use parentheses to indicate precedence.</p>
+          <p class="pt-3 font-bold text-lg">Rules for Keys:</p>
+          <p class="leading-5">You can specify multiple levels of key by seperating them with "."</p><span class="example">execution.uuid: "&lt;uuid&gt;"</span>
+          <p class="pt-3 leading-5">If your key contains a "." you must escape it with "\"</p><span class="example">key.contains.per\.iod: "value"</span>
+          <p class="pt-3 leading-5">You can combine multiple keys with AND and OR</p><span class="example">uuid: "&lt;uuid&gt;" OR (trunc_len: 150 AND hashed_feature_ids: true)</span>
+          <p class="pt-3 font-bold text-lg">Rules for Values:</p>
+          <p class="leading-5">If the value you are searching for is a string it must be in quotes</p><span class="example">type: "FeatureData"</span>
+          <p class="pt-3 leading-5"><span class="font-bold">NOTE:</span> the values true, false, and null are often NOT strings and do not need quotes</p>
+          <p class="pt-3 leading-5">Strings match on includes and all other types match on equality</p><span class="example">type: "FeatureData"</span><p class="leading-5">will match all types containing "FeatureData"</p><span class="example">sampling_depth: 1000</span><p class="leading-5">will only match sampling depths equaling exactly 1000</p>
+          <p class="pt-3 leading-5">Strings can use the start and end of string anchors "^" and "$"</p><span class="example">type: ^"FeatureData"</span><p class="leading-5">will match all types that start with "FeatureData"</p><span class="example">type: "[Taxonomy]"$</span><p class="leading-5">will match all types that end with "[Taxonomy]"</p><span class="example">type: ^"FeatureData[Taxonomy]"$</span><p class="leading-5">will only match exactly "FeatureData[Taxonomy]"</p>
+          <p class="pt-3 leading-5">If your search value contains a double quote, you will need to escape it with "\"</p><span class="example">type: "\""</span><p class="leading-5">will match all types that contain a double quote</p>
+          <p class="pt-3 leading-5">The same is true if your string contains a "\"</p><span class="example">type: "\\"</span><p class="leading-5">will match all types that contain a backslash</p>
+          <p class="pt-3 leading-5">Numerical values can use the comparison operators &gt;, &gt;=, &lt;, &lt;=</p><span class="example">sampling_depth: &gt;=1000</span><p class="leading-5">will match all sampling_depths greater than or equal to 1000</p>
+          <p class="pt-3 leading-5">You can also combine values with AND and OR. You must wrap these clauses in parentheses</p><span class="example">uuid: ((^"6" AND "5"$) OR "ee")</span><p class="leading-5">will match all uuids that start with 6 and end with 5 or contain "ee"</p>
+        </div>
+      {/if}
     {/if}
   </div>
 {/key}
