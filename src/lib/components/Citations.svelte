ced52ad5eae5ac36995f19f17ae415b2ee428d39
Oddant1 <oddant1@hotmail.com>
2025-07-07 10:44:02 -0700

    Steal melt modal but have it open on default then close permanently

--- src/lib/components/Citations.svelte
+++ src/lib/components/Citations.svelte
@@ -2,6 +2,27 @@
   import Panel from "$lib/components/Panel.svelte";
   import readerModel from "$lib/models/readerModel";
   import ResultDetails from "$lib/components/ResultDetails.svelte";
+  import { createDialog, melt } from '@melt-ui/svelte'
+  /** Internal helpers */
+  import { fade } from 'svelte/transition';
+    import { trusted } from "svelte/legacy";
+
+  let dismissed: boolean = $state(false);
+
+  const {
+    elements: {
+      trigger,
+      overlay,
+      content,
+      title,
+      description,
+      close,
+      portalled,
+    },
+    states: { open },
+  } = createDialog({
+    forceVisible: true,
+  });
 
   let renderedCitations: HTMLElement | undefined = $state();
 
@@ -32,6 +53,74 @@
     renderedCitations.innerHTML = newInnerHTML;
   }
 </script>
+
+{#if !dismissed && $readerModel.provenanceModel.highSeverityErrors.size > 0}
+  <div class="" use:melt={$portalled}>
+    <div
+      use:melt={$overlay}
+      class="fixed inset-0 z-50 bg-black/50"
+      transition:fade={{ duration: 150 }}
+    />
+    <div
+      class="fixed left-1/2 top-1/2 z-50 max-h-[85vh] w-[90vw]
+            max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-xl bg-white
+            p-6 shadow-lg"
+      use:melt={$content}
+    >
+      <h2 use:melt={$title} class="m-0 text-lg font-medium text-black">
+        Edit profile
+      </h2>
+      <p use:melt={$description} class="mb-5 mt-2 leading-normal text-zinc-600">
+        Make changes to your profile here. Click save when you're done.
+      </p>
+
+      <fieldset class="mb-4 flex items-center gap-5">
+        <label class="w-[90px] text-right text-black" for="name"> Name </label>
+        <input
+          class="inline-flex h-8 w-full flex-1 items-center justify-center
+                    rounded-sm border border-solid px-3 leading-none text-black"
+          id="name"
+          value="Thomas G. Lopes"
+        />
+      </fieldset>
+      <fieldset class="mb-4 flex items-center gap-5">
+        <label class="w-[90px] text-right text-black" for="username">
+          Username
+        </label>
+        <input
+          class="inline-flex h-8 w-full flex-1 items-center justify-center
+                    rounded-sm border border-solid px-3 leading-none text-black"
+          id="username"
+          value="@thomasglopes"
+        />
+      </fieldset>
+      <div class="mt-6 flex justify-end gap-4">
+        <button
+          onclick={() => dismissed = true}
+          class="inline-flex h-8 items-center justify-center rounded-sm
+                    bg-zinc-100 px-4 font-medium leading-none text-zinc-600"
+        >
+          Cancel
+        </button>
+        <button
+          onclick={() => dismissed = true}
+          class="inline-flex h-8 items-center justify-center rounded-sm
+                    bg-magnum-100 px-4 font-medium leading-none text-magnum-900"
+        >
+          Save changes
+        </button>
+      </div>
+      <button
+        onclick={() => dismissed = true}
+        aria-label="close"
+        class="absolute right-4 top-4 inline-flex h-6 w-6 appearance-none
+                items-center justify-center rounded-full p-1 text-magnum-800
+                hover:bg-magnum-100 focus:shadow-magnum-400"
+      >
+      </button>
+    </div>
+  </div>
+{/if}
 
 <Panel header="Details of {$readerModel.name}" customPanelClass="p-4 mb-4">
   <ResultDetails name={$readerModel.name} resultJSON={$readerModel.metadata}/>
