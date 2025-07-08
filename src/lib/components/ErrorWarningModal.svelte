6bdcef374537866378a66cc78c465858a74bc04a
Oddant1 <oddant1@hotmail.com>
2025-07-08 11:01:58 -0700

    Update wording and styling of modal

--- src/lib/components/ErrorWarningModal.svelte
+++ src/lib/components/ErrorWarningModal.svelte
@@ -30,15 +30,15 @@
     </div>
     <div
       class="fixed left-1/2 top-1/2 z-50 max-h-[85vh] w-[90vw]
-            max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white
-            p-4 shadow-lg"
+            max-w-[510px] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white
+            p-4 shadow-lg border border-gray-200"
       use:melt={$content}
     >
       <h2 use:melt={$title} class="text-lg">
-        The following critical errors were found in your provenance. Please inspect them in your "Provenance" tab.
+        The following high severity errors were found in your provenance. These may affect the validity of this Result. Please inspect them in your "Provenance" tab.
       </h2>
       {#each $readerModel.provenanceModel.highSeverityErrors as error}
-        <div class="px-2 my-2 bg-gray-200 rounded-md">
+        <div class="px-2 py-1 my-2 border border-gray-300 bg-gray-200 rounded-md">
           {error.name}
         </div>
       {/each}
@@ -46,14 +46,16 @@
         <button
           onclick={() => dismissed = true}
           class="inline-flex h-8 items-center justify-center rounded-md
-                bg-zinc-300 px-4 font-medium leading-none"
+                 border boder-zinc-400 bg-zinc-300 px-4 font-medium
+                 leading-none dismiss-button"
         >
           Dismiss
         </button>
         <button
           onclick={_redirectToProv}
           class="inline-flex h-8 items-center justify-center rounded-md
-                bg-blue-300 px-4 font-medium leading-none"
+                 border border-blue-400 bg-blue-300 px-4 font-medium
+                 leading-none view-button"
         >
           View Errors
         </button>
@@ -61,3 +63,13 @@
     </div>
   </div>
 {/if}
+
+<style lang="postcss">
+  .dismiss-button:hover {
+    @apply bg-zinc-400;
+  }
+
+  .view-button:hover {
+    @apply bg-blue-400;
+  }
+</style>
