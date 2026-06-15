99c1fca10923ff43d2d44436c2f2af1882f51cbc
Oddant1 <oddant1@hotmail.com>
2026-06-15 16:24:51 -0700

    Use better blue for background

--- src/lib/components/ProvErrors.svelte
+++ src/lib/components/ProvErrors.svelte
@@ -16,3 +16,12 @@
 {#if $readerModel.provenanceModel.errors.get(0) !== undefined}
   <ErrorDropdown errors={$readerModel.provenanceModel.errors.get(0)} svgPath={"/images/low-severity-error.svg"} />
 {/if}
+<!-- {#if $readerModel.provenanceModel.errors.get(2) !== undefined} -->
+  <ErrorDropdown errors={2} svgPath={"/images/hi-severity-error.svg"} />
+<!-- {/if} -->
+<!-- {#if $readerModel.provenanceModel.errors.get(1) !== undefined} -->
+  <ErrorDropdown errors={2} svgPath={"/images/medium-severity-error.svg"} />
+<!-- {/if} -->
+<!-- {#if $readerModel.provenanceModel.errors.get(0) !== undefined} -->
+  <ErrorDropdown errors={2} svgPath={"/images/low-severity-error.svg"} />
+<!-- {/if} -->
